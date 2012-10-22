722e1953f8c56910fce8fd08d3da5335fe630377
flaviatovo <flaviatovo@gmail.com>
2012-10-23 00:01:36 +0300

    Final version of kalloc, being able to set the number of bits dynamically

--- hw1/kalloc.c
+++ hw1/kalloc.c
@@ -10,10 +10,31 @@
 #include "spinlock.h"
 
 #define NUMBEROFPAGES 57069
+//#define BITS32
+#define BITS16
+//#define BITS8
+
+#ifdef BITS32
+////////////////////////////// 32 BITS ENTRY
 #define BITMAPARRAYSIZE 1784 // Number of 32 bits in entries on bitmap table
 #define BITMAPENTRYSIZE 32   // Number of bits per entry in the bitmap table
 #define BITMAPENTRYSIZEB 4   // Number of bytes in the bitmap entry
-                             // BITMAPENTRYSIZE/4
+                             // BITMAPENTRYSIZE/8
+#endif
+#ifdef BITS16
+////////////////////////////// 16 BITS ENTRY
+#define BITMAPARRAYSIZE 3567 // Number of 16 bits in entries on bitmap table
+#define BITMAPENTRYSIZE 16   // Number of bits per entry in the bitmap table
+#define BITMAPENTRYSIZEB 2   // Number of bytes in the bitmap entry
+                             // BITMAPENTRYSIZE/8
+#endif
+#ifdef BITS8
+////////////////////////////// 8 BITS ENTRY
+#define BITMAPARRAYSIZE 7134 // Number of 16 bits in entries on bitmap table
+#define BITMAPENTRYSIZE  8   // Number of bits per entry in the bitmap table
+#define BITMAPENTRYSIZEB 1   // Number of bytes in the bitmap entry
+                             // BITMAPENTRYSIZE/8
+#endif
 
 void initbitmaptable(void *vstart);
 extern char end[]; // first address after kernel loaded from ELF file
@@ -22,7 +43,20 @@ struct {
   struct spinlock lock;
   int use_lock;
   uint hint;
+
+#ifdef BITS8
+///////////// 8 BITS
+  uchar * bitmaptable;
+#endif
+#ifdef BITS16
+///////////// 16 BITS
+  uint * bitmaptable;
+#endif
+#ifdef BITS32
+///////////// 32 BITS
   unsigned long int * bitmaptable;
+#endif
+
 } kmem;
 
 // Initialization happens in two phases.
@@ -47,15 +81,42 @@ kinit2(void *vstart, void *vend)
 
 void
 initbitmaptable(void *vstart){
+
+#ifdef BITS8
+///////////// 8 BITS
+  kmem.bitmaptable = (uchar*)PGROUNDUP((uint)vstart);
+#endif
+#ifdef BITS16
+///////////// 16 BITS
+  kmem.bitmaptable = (uint*)PGROUNDUP((uint)vstart);
+#endif
+#ifdef BITS32
+///////////// 32 BITS
   kmem.bitmaptable = (unsigned long int*)PGROUNDUP((uint)vstart);
+#endif
 
   // Setting values
   // First 2 pages are used for the table
   kmem.bitmaptable[0] = 3;
   // Middle pages are free
   memset(kmem.bitmaptable + BITMAPENTRYSIZEB, 0, BITMAPARRAYSIZE*BITMAPENTRYSIZEB);
+
+#ifdef BITS8
+///////////// 8 BITS
+  // Last entry uses only 5 bits
+  kmem.bitmaptable[BITMAPARRAYSIZE-1] = 0xE0;
+#endif
+#ifdef BITS16
+///////////// 16 BITS
+  // Last entry uses only 13 bits
+  kmem.bitmaptable[BITMAPARRAYSIZE-1] = 0xE000;
+#endif
+#ifdef BITS32
+///////////// 32 BITS
   // Last entry uses only 13 bits
   kmem.bitmaptable[BITMAPARRAYSIZE-1] = 0xFFFFE000;
+#endif
+
 }
 
 //PAGEBREAK: 21
@@ -77,8 +138,22 @@ kfree(char *v)
 
   // Get correct possition inside bitmap
   uint temp = (((uint)v - (uint)kmem.bitmaptable)/PGSIZE);
+
+#ifdef BITS32
+///////////// 32 BITS
   index_in_bitmap = temp >> 5;
   index_in_entry = temp & 0x1F;
+#endif
+#ifdef BITS16
+///////////// 16 BITS
+  index_in_bitmap = temp >> 4;
+  index_in_entry = temp & 0xF;
+#endif
+#ifdef BITS8
+///////////// 8 BITS
+  index_in_bitmap = temp >> 3;
+  index_in_entry = temp & 0x7;
+#endif
 
   if(kmem.use_lock)
     acquire(&kmem.lock);
@@ -130,7 +205,18 @@ kalloc(void)
       index_in_entry ++;
     } while (chor_result);
 
+#ifdef BITS32
+///////////// 32 BITS
     address = ((index_in_bitmap << 5) + index_in_entry)*PGSIZE + (uint) kmem.bitmaptable;
+#endif
+#ifdef BITS16
+///////////// 16 BITS
+    address = ((index_in_bitmap << 4) + index_in_entry)*PGSIZE + (uint) kmem.bitmaptable;
+#endif
+#ifdef BITS8
+///////////// 8 BITS
+    address = ((index_in_bitmap << 3) + index_in_entry)*PGSIZE + (uint) kmem.bitmaptable;
+#endif
 
     kmem.bitmaptable[index_in_bitmap] |= 0x1 << index_in_entry;
 
