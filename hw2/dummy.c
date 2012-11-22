1fc894c305b0a563ea75a6da660f54ace512bab5
flaviatovo <flaviatovo@gmail.com>
2012-11-23 02:49:21 +0300

    test for pid

--- hw2/dummy.c
+++ hw2/dummy.c
@@ -5,25 +5,11 @@
 int
 main(int argc, char *argv[])
 {
-  uint i = 0xFFFF;
-  uchar * t1,* t2,* t3,* t4,* t5;
-
+  int pid;
   printf(1,"\nUsed pages: %d\n",pgused());
 
-  printf(1,"Malloc of %d \n", i);
-  t1 = (uchar *)malloc(i);
-  t2 = (uchar *)malloc(i);
-  t3 = (uchar *)malloc(i);
-  t4 = (uchar *)malloc(i);
-  t5 = (uchar *)malloc(i);
-  printf(1,"\nUsed pages: %d\n",pgused());
-  printf(1,"Free of %d \n", i);
-  free(t1);
-  free(t2);
-  free(t3);
-  free(t4);
-  free(t5);
-  printf(1,"\nUsed pages: %d\n",pgused());
+  pid = fork();
+  printf(1,"\nPID: %d\n",pid);
 
   exit();
 }
