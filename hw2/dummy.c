9d645b07a8a5bfcd60326338cacdad30987c9c6b
flaviatovo <flaviatovo@gmail.com>
2012-11-23 00:58:59 +0300

    test file (pgused)

--- hw2/dummy.c
+++ hw2/dummy.c
@@ -2,28 +2,13 @@
 #include "stat.h"
 #include "user.h"
 
-typedef unsigned long long ticks;
-
-static __inline__ ticks getticks(void)
-{
-     unsigned a, d;
-     asm("cpuid");
-     asm volatile("rdtsc" : "=a" (a), "=d" (d));
-
-//     printf(1,"\na: %d, d: %d\n",a,d);
-
-     return (((ticks)a) | (((ticks)d) << 32));
-}
-
-
 int
 main(int argc, char *argv[])
 {
   uint i = 0xFFFF;
   uchar * t1,* t2,* t3,* t4,* t5;
-  ticks tick,tick1;
 
-  tick = getticks();
+  printf(1,"\nUsed pages: %d\n",pgused());
 
   printf(1,"Malloc of %d \n", i);
   t1 = (uchar *)malloc(i);
@@ -31,18 +16,14 @@ main(int argc, char *argv[])
   t3 = (uchar *)malloc(i);
   t4 = (uchar *)malloc(i);
   t5 = (uchar *)malloc(i);
+  printf(1,"\nUsed pages: %d\n",pgused());
   printf(1,"Free of %d \n", i);
   free(t1);
   free(t2);
   free(t3);
   free(t4);
   free(t5);
-
-  tick1 = getticks();
-
-  printf(1,"\nStart time: %d\n",tick);
-  printf(1,"End time: %d\n",tick1);
-  printf(1,"Execution time: %d\n",tick1-tick);
+  printf(1,"\nUsed pages: %d\n",pgused());
 
   exit();
 }
